bd7c584d281721d8fbfc445e18368054531dbb43
Nick Zhu <xz05me@gmail.com>
2016-09-22 21:08:02 +0800

    9.22打卡
    重复昨天的故事

---
+++ DoodleFun/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  DoodleFun
+//
+//  Created by Nick Zhu on 2016/9/22.
+//  Copyright © 2016年 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+}
--- DoodleFun/ViewController.swift
+++ DoodleFun/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
