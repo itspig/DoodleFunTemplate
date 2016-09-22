247b0576b807f645c3bc26771636118b3025c7ab
Nick Zhu <xz05me@gmail.com>
2016-09-22 20:50:32 +0800

    init

---
+++ DoodleFun/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DoodleFun
+//
+//  Created by Simon Ng on 19/1/15.
+//  Copyright (c) 2015 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
